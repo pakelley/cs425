4db81b360843a5dfc1b774c22577b34043a12cdf
Brian Gaunt <bgaunt@nevada.unr.edu>
2017-05-04 16:09:19 -0700

    created functions and error checked

--- pyCollectData.py
+++ pyCollectData.py
@@ -12,16 +12,18 @@ import atexit
 from signal import SIGTERM
 import numpy as np
 
-#---------- GLOBAL SETTINGS VARIABLES ---------------
+#---------- GLOBAL SETTINGS VARIABLES ----------------------
 
-TIMEINTERVAL = 20
+TIMEINTERVAL = 2
 #timeinterval is needed so that the time between
 #datetime.now() and the query is not
 #infinitesimally small - measured in seconds
 
-BLOBNUMBER = 4 #unused as of yet
+BLOBNUMBER = 4
+#The number of 4-set sensor blobs allowed
+#If BLOBNUMBER = 5, then 20 sensor blobs will be collected
 
-#----------------------------------------------------
+#-----------------------------------------------------------
 
 class SampleRow(object):
     '''Row of Data from Query with Blob Cracking'''
@@ -45,6 +47,26 @@ class SampleRow(object):
             i = i+2
             j = j+2
 
+
+def getUTCtime():
+    now = datetime.datetime.now() - datetime.timedelta(seconds=TIMEINTERVAL)
+    utctime = str(now + datetime.timedelta(hours=7))
+    #utctime = "2017-05-04 21:16:00" #------------ For Testing
+    return utctime[:-3]
+
+def executeQuery(cursor, utctime):
+    qcommand = "select segment_id,gmt,start_rev_rpm,sample_data from dbo.waveform where (segment_id = 218 or segment_id = 244 or segment_id = 270 or segment_id = 296) and gmt > '" + str(utctime) + "' order by gmt, segment_id"
+    cursor.execute(qcommand)
+
+def checkRowLoopValid(rowCount, classRow):
+    if  ((rowCount % 4 == 0 and classRow.segment_id != 218) or
+        (rowCount % 4 == 1 and classRow.segment_id != 244) or
+        (rowCount % 4 == 2 and classRow.segment_id != 270) or
+        (rowCount % 4 == 3 and classRow.segment_id != 296)):
+        print "INVADLID DATA-------------------------------------------- CONSECUTIVE IDs ---------------"
+        return False
+    return True
+
 class RunForever(object):
     def __init__(self):
         self.run()
@@ -54,31 +76,85 @@ class RunForever(object):
                                 "Database=System1_Hist;"
                                 "Trusted_Connection=yes;")
         cursor = conn.cursor()
+        if not cursor:
+            print "Cannot Connect to SQL Database - Exiting Program"
+            return
+        else:
+            print "program starting"
 
-        print "starting"
-        count = 0
-        while(count < 1):
-            classRowList = []
-            #List of Each Sample in Query stored in the SampleRow class
-            now = datetime.datetime.now() - datetime.timedelta(seconds=220)
-            utctime = str(now + datetime.timedelta(hours=7))
-            utctime = utctime[:-3]
-            qcommand = "select segment_id,gmt,start_rev_rpm,sample_data from dbo.waveform where (segment_id = 218 or segment_id = 244 or segment_id = 270 or segment_id = 296) and gmt > '" + str(utctime) + "' order by gmt"
-            cursor.execute(qcommand)
+        toggle = True
+        while(1):
+
+            '''
+            -----------WHAT EVERYONE ELSE NEEDS-------------
+            ONLY np arrays of each sensor blob
+            ALWAYS will be a multiple of 4
+            ALWAYS will be ordered in ascending segment_id's
+            1st = sid 218 - Motor Side X-Sensor
+            2nd = sid 244 - Motor Side Y-Sensor
+            3rd = sid 270 - Outer Side X-Sensor
+            4th = sid 296 - Outer Side Y-Sensor
+            '''
+            dataPassingList = []
+
+            #FULL information on each sensor blob row
+            classRowList = [] #WHAT BRIAN NEEDS
+
+            #Gets UTC time from Actual Time for use in SQL Query
+            utctime = getUTCtime()
+
+            #Runs the query
+            executeQuery(cursor, utctime)
+
             row = cursor.fetchone()
+            if row:
+                #print " "
+                #print "Query FULL"
+                #print " "
+                toggle = True
+
             rowCount = 0
-            while row and rowCount<BLOBNUMBER*4:
+            valid = True
+
+            while row and rowCount<BLOBNUMBER*4 and valid:
                 classRow = SampleRow(row[0],row[1],row[2],row[3],0)
                 classRow.crackHexData()
                 classRow.time = classRow.time - datetime.timedelta(hours=7)
                 data = np.array(classRow.crackedData)
-                rowTup = [data, classRow.segment_id]
-                classRowList.extend([rowTup])
+
+                valid = checkRowLoopValid(rowCount, classRow)
+
+                sensorBlob = [data, classRow.segment_id]
+                classRowList.extend([sensorBlob])
                 rowCount = rowCount + 1
+                #writer.writerow([rowTup])
                 row = cursor.fetchone()
-            for p in classRowList: print p
-            count = count + 1
-        print "done"
+
+            if not classRowList and toggle:
+                #print " "
+                #print "QUERY EMPTY"
+                #print " "
+                toggle = False
+            elif rowCount % 4 != 0:
+                print "INVADLID DATA-------------------------------------------- NOT GROUPED BY 4 ---------------"
+            else:
+                for p in classRowList:
+                    dataPassingList.extend([p[0]])
+                    #print p[0]
+
+
+            '''
+
+
+            CLASSIFIER CODE
+
+
+            '''
+
+
+        print "program ending"
         conn.close()
 
+
+#Run Daemon
 RunForever()
